3381f1193a80efa25a981b99b55d9642164dd5fe
robinkii <littlerobin@bandgeek.dev>
2024-07-05 20:40:05 -0400

    updated user model to use manager

--- main/models.py
+++ main/models.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .managers import CustomUserManager
 
 # User model
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=50, unique=True)
-    password_hash = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.username
