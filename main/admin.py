fdb7817dce4de6a5488aa2c0ea1c7b309724c7ff
robinkii <littlerobin@bandgeek.dev>
2024-07-05 21:11:22 -0400

    added password has code

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser, Profile, Genre, Show, Episode, Movie, Review, SubscriptionType, Subscription, ViewingHistory, Watchlist
 
